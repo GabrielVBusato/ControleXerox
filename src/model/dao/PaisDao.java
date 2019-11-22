921ce5fd263d0bad63f0244710bef9a508229e23
GabrielVBusato <busatinbr@gmail.com>
2019-11-22 18:10:43 -0300

    connection

--- src/model/dao/PaisDao.java
+++ src/model/dao/PaisDao.java
@@ -5,10 +5,42 @@
  */
 package model.dao;
 
+import connection.ConnectionFactory;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+import model.bean.Cidade;
+
 /**
  *
  * @author Gabriel
  */
 public class PaisDao {
-    
+
+    public static void inserir(String nome) {
+        Connection con = ConnectionFactory.getConnection();
+        PreparedStatement stmt = null;
+        String sql = "INSERT INTO pais(nome) values (?)";
+
+        try {
+            //Preparando statement e retornando a ultima chave primaria inserida
+            stmt = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+
+            //Setando os valores
+            stmt.setString(1, nome);
+
+            //Execuntando comando de inserção no banco
+            stmt.executeUpdate();
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, ex);
+
+        } finally {
+            ConnectionFactory.closeConnection(con, stmt);
+        }
+    }
+
 }
